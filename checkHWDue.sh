de73a5cbd3b6055d806c7c0ea07671e666ae51fe
Daniel Wang <s92025592025@gmail.com>
2017-01-11 11:22:59 -0800

    Add testing result of direct curling

--- checkHWDue.sh
+++ checkHWDue.sh
@@ -1,3 +1,4 @@
 #Reference for tetsing
 #http://superuser.com/questions/640839/how-to-enter-login-information-for-a-website-from-the-linux-command-line
-#
+
+# If directly curl to canvas assignment page, will only show "Redirected"
